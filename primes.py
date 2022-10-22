ee5cec4ca3c7891269b35c3bee201af8c36b3307
Alexandru Godoroja <alexandru.godoroja@kcl.ac.uk>
2022-10-22 19:56:12 +0100

    list exercise solved

--- primes.py
+++ primes.py
@@ -1,6 +1,21 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
+import math 
 
 def primes(number_of_primes):
     list = []
+    count = 2
+    
+    while len(list)<number_of_primes:
+        isprime = True
+        
+        for x in range(2, int(math.sqrt(count) + 1)):
+            if count % x == 0: 
+                isprime = False
+                break
+        
+        if isprime:
+            list.append(count)
+        
+        count += 1
     return list
